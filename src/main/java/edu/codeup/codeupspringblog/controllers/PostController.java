9178d5919156af53d3403db2a435930200887d9f
Donald Twitty <donaldctwittyjr@outlook.com>
2023-09-14 16:22:53 -0500

    completed PostController Class

--- src/main/java/edu/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/edu/codeup/codeupspringblog/controllers/PostController.java
@@ -1,4 +1,26 @@
 package edu.codeup.codeupspringblog.controllers;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class PostController {
+    @GetMapping("/posts")
+    public String getPostsIndexPage() {
+        return "This is the posts index page.";
+    }
+
+    @GetMapping("posts/{id}")
+    public String viewPost(@PathVariable("id") String id) {
+        return "This is the page for viewing post with id " + id;
+    }
+
+    @GetMapping("posts/create")
+    public String getPostCreateForm() {
+        return "View form for creating a post.";
+    }
+
+    @PostMapping("posts/create")
+    public String createPost() {
+        return "This is a new post.";
+    }
 }
