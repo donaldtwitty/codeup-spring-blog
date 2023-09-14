f1ebf9de23709d2c7f3b39063b39543a1471169a
Donald Twitty <donaldctwittyjr@outlook.com>
2023-09-14 15:36:49 -0500

    completed exercise number one

---
+++ src/main/java/edu/codeup/codeupspringblog/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package edu.codeup.codeupspringblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping("/{page}")
+    @ResponseBody
+    public String landingPage(@PathVariable String page) {
+        return String.format("Hello this is the landing,  %s!", page);
+    }
+}
