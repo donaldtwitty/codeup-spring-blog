6eb2fa3e51713bfb7053d77b783ddf8f1b93218e
Donald Twitty <donaldctwittyjr@outlook.com>
2023-09-14 16:04:17 -0500

    completed MathController Class

--- src/main/java/edu/codeup/codeupspringblog/controllers/MathController.java
+++ src/main/java/edu/codeup/codeupspringblog/controllers/MathController.java
@@ -1,4 +1,27 @@
 package edu.codeup.codeupspringblog.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MathController {
+    @GetMapping("/add/{firstNumber}/{secondNumber}")
+    public int add(@PathVariable("firstNumber") int firstNumber, @PathVariable("secondNumber") int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+    @GetMapping("/subtract/{firstNumber}/{secondNumber}")
+    public int subtract(@PathVariable("firstNumber") int firstNumber, @PathVariable("secondNumber") int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    @GetMapping("/multiply/{firstNumber}/{secondNumber}")
+    public int multiply(@PathVariable("firstNumber") int firstNumber, @PathVariable("secondNumber") int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    @GetMapping("/divide/{firstNumber}/{secondNumber}")
+    public int divide(@PathVariable("firstNumber") int firstNumber, @PathVariable("secondNumber") int secondNumber) {
+        return firstNumber / secondNumber;
+    }
 }
